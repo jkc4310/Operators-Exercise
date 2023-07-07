976c75ca4cc5cd13c247d83f0b8da29ea74cb526
kendall chapman <jkc4310@gmail.com>
2023-07-06 20:50:00 -0400

    exercise two commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,15 +2,36 @@
 {
     public class Program
     {
+        
+
         static void Main(string[] args)
-        {
+        { //-----------------Exercise 1-----------------//
             var a = 17;
             var b = 4;
             var quotient = a / b;
             var remainder = a % b;
 
-            Console.WriteLine(a + " / " + b + " is " + quotient + " remainder " + remainder);
+            
             Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("Please enter the radius of your circle:");
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaOfCircle(radius));
+
+
+
         }
-    }
+
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+            return area;
+        }
+    } 
+
+        
+    
 }
